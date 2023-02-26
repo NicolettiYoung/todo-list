c9026b6ebe24bba4113bee8d449084f3378cdaa0
NicolettiYoung <s0988755325@gmail.com>
2023-02-26 13:55:13 +0800

    feat: model todo add isDone params

--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const todoSchema = new Schema({
   name: {
     type: String,
     required: true
   },
-  done: {
-    type: Boolean
+  isDone: {
+    type: Boolean,
+    default: false//表紀錄產生時，會設定初始狀態為"未完成"
   }
 })
+
 module.exports = mongoose.model('Todo', todoSchema)
