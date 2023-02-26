730065be6358bfdbbded00f56d28f738743d46fb
NicolettiYoung <s0988755325@gmail.com>
2023-02-26 14:10:32 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({ extend: true }))
 app.get('/', (req, res) => {
   Todo.find()//取出Todo model裡的所有資料
     .lean()//把Mongoose的Model物件轉換成乾淨的JavaScripts資料陣列
+    .sort({ _id: 'asc' })//根據_id升冪排序
     .then(todos => res.render('index', { todos }))//將資料傳給index樣板
     .catch(error => console.error(error))//錯誤處理
 })
