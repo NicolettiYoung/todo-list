ae284e6cd906efb32cb3b9e441940f729272114d
NicolettiYoung <s0988755325@gmail.com>
2023-02-26 16:09:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
-const Todo = require('./models/todo')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -35,67 +21,6 @@ app.use(bodyParser.urlencoded({ extend: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
-// app.get('/', (req, res) => {
-//   Todo.find()//取出Todo model裡的所有資料
-//     .lean()//把Mongoose的Model物件轉換成乾淨的JavaScripts資料陣列
-//     .sort({ _id: 'asc' })//根據_id升冪排序
-//     .then(todos => res.render('index', { todos }))//將資料傳給index樣板
-//     .catch(error => console.error(error))//錯誤處理
-// })
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  // return Todo.create({ name })
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
-
-  const todo = new Todo({ name })
-  return todo.save()
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
